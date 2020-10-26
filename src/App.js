f92370a049557edb2b3f9ab04df1d6c6a8a98659
samSterns <sam.g.sterns@gmail.com>
2020-10-26 13:14:02 -0700

    tool-tip needed to be imported

--- src/App.js
+++ src/App.js
@@ -1,27 +1,43 @@
-import React from "react";
-// import ReactDOM from "react-dom";
-import { ComposableMap, Geographies, Geography } from "react-simple-maps";
+import React, { useState } from "react";
+import ReactDOM from "react-dom";
+import ReactTooltip from "react-tooltip";
 
 import "./styles.css";
 
-const geoUrl =
-  "https://raw.githubusercontent.com/zcreativelabs/react-simple-maps/master/topojson-maps/world-110m.json";
+import MapChart from "./MapChart";
 
-const App = () => (
-  <div>
-    <ComposableMap>
-      <Geographies geography={geoUrl}>
-        {({ geographies }) =>
-          geographies.map(geo => <Geography key={geo.rsmKey} geography={geo} />)
-        }
-      </Geographies>
-    </ComposableMap>
-  </div>
-);
+function App() {
+  const [content, setContent] = useState("");
+  return (
+    <div>
+      <MapChart setTooltipContent={setContent} />
+      <ReactTooltip>{content}</ReactTooltip>
+    </div>
+  );
+}
+
+export default App;
+// import React from "react";
+// // import ReactDOM from "react-dom";
+// import { ComposableMap, Geographies, Geography } from "react-simple-maps";
+
+// import "./styles.css";
+
+// const geoUrl =
+//   "https://raw.githubusercontent.com/zcreativelabs/react-simple-maps/master/topojson-maps/world-110m.json";
+
+// const App = () => (
+//   <div>
+//     <ComposableMap>
+//       <Geographies geography={geoUrl}>
+//         {({ geographies }) =>
+//           geographies.map(geo => <Geography key={geo.rsmKey} geography={geo} />)
+//         }
+//       </Geographies>
+//     </ComposableMap>
+//   </div>
+// );
 
 // const rootElement = document.getElementById("root");
 // ReactDOM.render(<App />, rootElement);
 
-
-
-export default App;
